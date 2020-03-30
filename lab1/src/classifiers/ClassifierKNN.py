ef37c7142cf60233171863c356ab9dfbd9c0c490
kaczor6418 <andrzej6418@gmail.com>
2020-03-31 00:02:47 +0200

    wip(KNN): implementing KNN classifier

--- lab1/src/classifiers/ClassifierKNN.py
+++ lab1/src/classifiers/ClassifierKNN.py
@@ -1,26 +1,23 @@
 import math
-from typing import List
+from typing import List, Dict
 
 from src.Leaf import Leaf
 from src.Point import Point
+from src.classifiers.ClassifierNN import ClassifierNN
 from src.classifiers.typedDictionaries.NearestElement import NearestElement
 
 
-class ClassifierNN:
+class ClassifierKNN:
 
     @staticmethod
     def classify(train_group: List[Leaf], test_element: Point, trait_x_id: int, trait_y_id: int) -> int:
         nearest_element_id_distance: NearestElement = ClassifierNN.create_nearest_element(-1, math.inf)
+        elements_id_distances: Dict[int, List[float]] = dict()
         for train_element in train_group:
             train_element_point: Point = train_element.create_point_from_traits(trait_x_id, trait_y_id)
             distance: float = test_element.calculate_distance_to_point(train_element_point)
-            if distance < nearest_element_id_distance['distance']:
-                nearest_element_id_distance = ClassifierNN.create_nearest_element(train_element.leaf_name_id, distance)
-        return nearest_element_id_distance['leaf_name_id']
-
-    @staticmethod
-    def create_nearest_element(element_id: int, distance: float) -> NearestElement:
-        return {
-            'leaf_name_id': element_id,
-            'distance': distance
-        }
+            if elements_id_distances.get(train_element.leaf_name_id) is None:
+                elements_id_distances[train_element.leaf_name_id] = [distance]
+            else:
+                elements_id_distances.get(train_element.leaf_name_id).append(distance)
+        for element_id_distance in elements_id_distances:
---
+++ lab1/src/classifiers/typedDictionaries/NearestElement.py
@@ -0,0 +1,6 @@
+from typing import TypedDict
+
+
+class NearestElement(TypedDict):
+    leaf_name_id: int
+    distance: float
