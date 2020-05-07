11cc2fc90a164217339d45151ec7e406b9a7dbf9
kaczor6418 <andrzej6418@gmail.com>
2020-05-07 22:34:01 +0200

    feat(NM): working NM classifier

--- lab1/main.py
+++ lab1/main.py
@@ -4,7 +4,22 @@ from src.converters.ConverterJsonAnalyzerConfig import ConverterJsonToAnalyzerCo
 from src.dto.AnalyzerSchemaDTO import AnalyzerSchemaDTO
 from src.loaders.JsonFileLodaer import JsonFileLoader
 
-json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file('configs/cfg-knn-euclides-k=3-class=[3,5]-traits=[4,7].json')
-converted_config: AnalyzerSchema = ConverterJsonToAnalyzerConfig.to_analyzer_config(json_config)
-analyzer: Analyzer = Analyzer(converted_config)
-analyzer.run_analysis()
+
+def knn_analysis() -> None:
+    json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file(
+        'configs/cfg-knn-euclides-k=3-class=[3,5]-traits=[4,7].json')
+    converted_config: AnalyzerSchema = ConverterJsonToAnalyzerConfig.to_analyzer_config(json_config)
+    analyzer: Analyzer = Analyzer(converted_config)
+    analyzer.run_analysis()
+
+
+def nm_analysis() -> None:
+    json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file(
+        'configs/cfg-nm-euclides-class=[3, 5, 7, 9]-traits=[all].json')
+    converted_config: AnalyzerSchema = ConverterJsonToAnalyzerConfig.to_analyzer_config(json_config)
+    analyzer: Analyzer = Analyzer(converted_config)
+    analyzer.run_analysis()
+
+
+knn_analysis()
+nm_analysis()
--- lab1/src/classifiers/ClassifierNM.py
+++ lab1/src/classifiers/ClassifierNM.py
@@ -16,29 +16,32 @@ class ClassifierNM(AbstractClassifier):
     def __init__(self, train_group: List[DatasetElement], compared_traits: List[int],
                  calculator_type: CalculatorType, classes_ids: List[int]) -> None:
         super().__init__(train_group, compared_traits, calculator_type)
-        self.set_average_positions(compared_traits, classes_ids)
+        self.set_average_positions(classes_ids)
 
-    @staticmethod
-    def get_traits_values_from_dataset(dataset: DatasetElement, compared_traits: List[int]) -> np.ndarray:
-        trait_values: List[float] = []
-        for trait in compared_traits:
-            trait_values.append(dataset.metadata[trait])
-        return np.array(trait_values)
-
-    def set_average_positions(self, compared_traits: List[int], classes_ids: List[int]):
+    def set_average_positions(self, classes_ids: List[int]):
+        class_number_of_occurrences: Dict[int, int] = dict()
         for dataset in self.train_group:
             if dataset.element_class_id in classes_ids:
-                trait_values: np.ndarray = self.get_traits_values_from_dataset(dataset, compared_traits)
+                trait_values: np.ndarray = ClassifiersUtils.get_values_by_indexes(dataset.metadata,
+                                                                                  self.compared_traits)
                 if dataset.element_class_id in self.average_classes_positions:
                     current_values: np.ndarray = self.average_classes_positions[dataset.element_class_id]
                     self.average_classes_positions[dataset.element_class_id] = np.add(current_values, trait_values)
+                    class_number_of_occurrences[dataset.element_class_id] += 1
                 else:
                     self.average_classes_positions[dataset.element_class_id] = trait_values
+                    class_number_of_occurrences[dataset.element_class_id] = 1
+        for class_id, class_average_positions in self.average_classes_positions.items():
+            number_of_occurrences = class_number_of_occurrences[class_id]
+            for i in range(len(class_average_positions)):
+                class_average_positions[i] = class_average_positions[i] / number_of_occurrences
 
     def classify(self, test_element: np.ndarray) -> int:
         nearest_element_id_distance: NearestElement = ClassifiersUtils.create_nearest_element(-1, math.inf)
+        test_traits_values: np.ndarray = ClassifiersUtils.get_values_by_indexes(test_element,
+                                                                                self.compared_traits)
         for class_id, class_average_position in self.average_classes_positions.items():
-            distance: float = self.calculator.calculate_similarity(test_element, class_average_position)
+            distance: float = self.calculator.calculate_similarity(test_traits_values, class_average_position)
             if distance < nearest_element_id_distance['distance']:
                 nearest_element_id_distance = ClassifiersUtils.create_nearest_element(class_id, distance)
         return nearest_element_id_distance['element_class_id']
