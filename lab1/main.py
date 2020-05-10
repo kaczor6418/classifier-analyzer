b797aef4add192f31fe06f8aef7491623adb151c
kaczor6418 <andrzej6418@gmail.com>
2020-05-10 22:36:36 +0200

    chore(config): add some configs

--- lab1/main.py
+++ lab1/main.py
@@ -5,21 +5,17 @@ from src.dto.AnalyzerSchemaDTO import AnalyzerSchemaDTO
 from src.loaders.JsonFileLodaer import JsonFileLoader
 
 
-def knn_analysis() -> None:
-    json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file(
-        'configs/cfg-knn-euclides-k=3-class=[3,5,7]-traits=[4,7].json')
+def create_analysis(config_path: str) -> None:
+    json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file(config_path)
     converted_config: AnalyzerSchema = ConverterJsonToAnalyzerConfig.to_analyzer_config(json_config)
     analyzer: Analyzer = Analyzer(converted_config)
     analyzer.run_analysis()
+    analyzer.show_results()
 
 
-def nm_analysis() -> None:
-    json_config: AnalyzerSchemaDTO = JsonFileLoader.load_file(
-        'configs/cfg-nm-euclides-class=[3,5,7]-traits=[all].json')
-    converted_config: AnalyzerSchema = ConverterJsonToAnalyzerConfig.to_analyzer_config(json_config)
-    analyzer: Analyzer = Analyzer(converted_config)
-    analyzer.run_analysis()
-
-
-knn_analysis()
-nm_analysis()
+create_analysis('config/cfg-nn-euclides-class=[3,5,7]-traits=[all].json')
+create_analysis('config/cfg-knn-euclides-k=3-class=[3,5,7]-traits=[all].json')
+create_analysis('config/cfg-nm-euclides-class=[3,5,7]-traits=[all].json')
+create_analysis('config/cfg-nn-deviation-class=[3,5,7]-traits=[all].json')
+create_analysis('config/cfg-knn-deviation-k=3-class=[3,5,7]-traits=[all].json')
+create_analysis('config/cfg-nm-deviation-class=[3,5,7]-traits=[all].json')
